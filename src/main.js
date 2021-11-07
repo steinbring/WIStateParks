dcb965c099b31c322b96d8c9a77c423d22c7b11b
Joe Steinbring <joe@jws.app>
2021-11-07 08:33:05 -0600

    I added vue router

--- src/main.js
+++ src/main.js
@@ -6,9 +6,11 @@ import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import '@fortawesome/fontawesome-pro/css/all.css'
 import '@fortawesome/fontawesome-pro/js/all.js'
+import VueRouter from 'vue-router'
 
 Vue.use(VueAxios, axios)
 Vue.use(Buefy)
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
